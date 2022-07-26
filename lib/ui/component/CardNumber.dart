58f586b6c63dba7e9a4326141259cba51e3c25e2
TAHAR Noureddine <71546633+EngrTAHAR-Noureddine@users.noreply.github.com>
2022-07-26 15:36:26 +0100

    update change autoGenerate color function

--- lib/ui/component/CardNumber.dart
+++ lib/ui/component/CardNumber.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../constant/custom_color.dart';
+
 
 Widget cardNumber({required int number , required double side}){
 
   return Container(
     height: side,
     width: side,
+    padding: const EdgeInsets.all(5),
     decoration:  BoxDecoration(
-      color:  Colors.blueAccent, //Color((number.toDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+      color: (number>0)?Color(int.parse(number.toString(), radix: 16) * initColorCard):Colors.transparent,
       borderRadius: const BorderRadius.all(Radius.circular(5)),
 
     ),
     child: FittedBox(
       fit: BoxFit.contain,
-      child: Text(number.toString(), style:const TextStyle(color: Colors.white),),
+      child: Text(number.toString(), style: TextStyle(color: (number > 0)?textColorCard: Colors.transparent),),
     ),
 
   );
