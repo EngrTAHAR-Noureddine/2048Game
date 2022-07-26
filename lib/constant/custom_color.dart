8c1fcef187d507db4c231b9830cfec7048d4cd8a
TAHAR Noureddine <71546633+EngrTAHAR-Noureddine@users.noreply.github.com>
2022-07-26 15:35:51 +0100

    add colors

---
+++ lib/constant/custom_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color boardColor = Color(0xFFDDFFF7);
+const int initColorCard = 0xFF009DDC;
+const Color textColorCard = Color(0xFFFFFFFF);
+const Color backgroundColor = Color(0xFFFCFCFC);
+const Color headerColor = Color(0xFF30292F);
+const Color textColor = Color(0xFF3F4045);
